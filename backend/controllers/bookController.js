9e3f55b9191dff118489759fb354fb6329e75b23
men21049 <men21049@byui.edu>
2025-07-31 20:43:12 -0600

    fixed extra module.exports in bookconrtollers

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -3,29 +3,28 @@ const ObjectId = require("mongodb").ObjectId;
 const { bookValidation } = require("../utils/validation");
 
 const getAllBooks = async (req, res) => {
-
-    try {
-        const result = await mongodb.getDb.db().collection("books");
-        const books = await result.toArray();
-        res.status(200).json(books);
-    } catch (err) {
-        res.status(500).json({error: "Error fetching the books", details: err});
-    }
-}
+  try {
+    const result = await mongodb.getDb.db().collection("books");
+    const books = await result.toArray();
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching the books", details: err });
+  }
+};
 
 const getSingleBook = async (req, res) => {
-
-    try {
-        bookId = new ObjectId(req.params.id);
-        const result = await mongodb.getDb.db().collection("books").find({_id: bookId});
-        const books = await result.toArray();
-        res.status(200).json(books);
-    } catch (err) {
-        res.status(500).json({error: "Error fetching the books", details: err});
-    }
-}
-
-module.exports = {getAllBooks, getSingleBook};
+  try {
+    bookId = new ObjectId(req.params.id);
+    const result = await mongodb.getDb
+      .db()
+      .collection("books")
+      .find({ _id: bookId });
+    const books = await result.toArray();
+    res.status(200).json(books);
+  } catch (err) {
+    res.status(500).json({ error: "Error fetching the books", details: err });
+  }
+};
 
 const createBook = async (req, res) => {
   //#swagger.tag=['Books']
@@ -110,6 +109,8 @@ const deleteBook = async (req, res) => {
 };
 
 module.exports = {
+  getAllBooks,
+  getSingleBook,
   createBook,
   updateBook,
   deleteBook,
