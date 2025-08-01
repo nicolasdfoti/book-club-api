8e583fa7cb10b6c9baf0fe949130bfc7a1be2ca6
Nicolás Foti <nicolasdfoti@gmail.com>
2025-07-31 23:20:23 -0300

    added get routes

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -2,6 +2,31 @@ const mongodb = require("../data/db");
 const ObjectId = require("mongodb").ObjectId;
 const { bookValidation } = require("../utils/validation");
 
+const getAllBooks = async (req, res) => {
+
+    try {
+        const result = await mongodb.getDb.db().collection("books");
+        const books = await result.toArray();
+        res.status(200).json(books);
+    } catch (err) {
+        res.status(500).json({error: "Error fetching the books", details: err});
+    }
+}
+
+const getSingleBook = async (req, res) => {
+
+    try {
+        bookId = new ObjectId(req.params.id);
+        const result = await mongodb.getDb.db().collection("books").find({_id: bookId});
+        const books = await result.toArray();
+        res.status(200).json(books);
+    } catch (err) {
+        res.status(500).json({error: "Error fetching the books", details: err});
+    }
+}
+
+module.exports = {getAllBooks, getSingleBook};
+
 const createBook = async (req, res) => {
   //#swagger.tag=['Books']
   const book = {
